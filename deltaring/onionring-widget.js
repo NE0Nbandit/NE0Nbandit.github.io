968d0d721c956523eff020fe990d5983747a0f60
Jay <77340570+NE0Nbandit@users.noreply.github.com>
2023-07-28 20:41:17 +0100

    Update onionring-widget.js

--- deltaring/onionring-widget.js
+++ deltaring/onionring-widget.js
@@ -28,7 +28,7 @@ function randomSite() {
 //if we didn't find the site in the list, the widget displays a warning instead
 if (thisIndex == null) {
   tag.insertAdjacentHTML('afterbegin', `
-<link rel="stylesheet" href="https://ne0nbandit.github.io/Deltaring/onionring.css">
+<link rel="stylesheet" href="https://ne0nbandit.github.io/deltaring/onionring.css">
 <table>
   <tr>
     <td class='webring-info'>
@@ -62,7 +62,7 @@ else {
 
   //this is the code that displays the widget - EDIT THIS if you want to change the structure
   tag.insertAdjacentHTML('afterbegin', `
-  <link rel="stylesheet" href="https://ne0nbandit.github.io/Deltaring/onionring.css">
+  <link rel="stylesheet" href="https://ne0nbandit.github.io/deltaring/onionring.css">
   <table>
     <tr>
       <td class='webring-info'>
